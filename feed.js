1a9631552e1a71bc10661527b2b743be58e21c6b
furuhiro <kanakana1610@gmail.com>
2016-07-13 05:54:47 +0900

    webサイト版のRSSリーダの作成

--- feed.js
+++ feed.js
@@ -1,10 +1,13 @@
 google.load("feeds", "1");
 
 function initialize() {
+  //rssリーダーのインスタンス化
   var control = new google.feeds.FeedControl();
-  var feedurl1 = "http://d.hatena.ne.jp/mltmdkana/rss";
+  var feedurl1 = "http://gigazine.net/news/rss_2.0/";//GigazineのRSSフィード取得
+  var feedurl2 = "http://tabi-labo.com/feed/";//TABILABOのRSSフィードの取得
 
-  control.addFeed(feedurl1, "HOGE3");
+  control.addFeed(feedurl1, "Gigazine");
+  control.addFeed(feedurl2, "TABILABO");
   control.draw(document.getElementById("feed"));
 }
 google.setOnLoadCallback(initialize);
